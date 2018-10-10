26685acb29d2a1e470d313706d0b9299424be48d
ANTONELLA\anton <breton.designin@gmail.com>
2018-10-10 08:46:10 -0700

    Changes

--- Environment Display/src/main.cpp
+++ Environment Display/src/main.cpp
@@ -1,33 +1,20 @@
 #include <Arduino.h>
-#include <TinyOzOLED.h>
-#include <TinyWireM.h>
 
 
-
-long i=0;
-byte brightness = 0;
+int outputFreq = 4;
+unsigned long timeUS = 0;
 
 void setup() {
     // put your setup code here, to run once:
-    OzOled.init();
-    OzOled.setInverseDisplay(); // Invert the display
-    delay(5000);
-    OzOled.printString("Hi There");
-    delay(5000); 
-    OzOled.clearDisplay();
-    OzOled.printString("Brightness:", 0, 2);
-    delay(1000);
+  pinMode(outputFreq, OUTPUT);
 
 }
 
 void loop() {
     // put your main code here, to run repeatedly:
-    OzOled.setBrightness(brightness++);
-    OzOled.printNumber((long)brightness, 12, 2);
-    delay(100);
-
-    if(brightness >= 255){
-        brightness = 0;
-        OzOled.printString("   ", 12, 2);
-    }
+    timeUS = micros();
+    digitalWrite(outputFreq, HIGH);
+    delayMicroseconds(3); 
+    digitalWrite(outputFreq, LOW);
+    delayMicroseconds(3);
 }
